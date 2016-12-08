2e7e87134eb6b7e17e83331ab60d061c64397edc
Mariano SDC <mscenzano@gmail.com>
2016-12-08 13:29:18 +0100

    Clients controller created CRUD created

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,61 @@
 class ClientsController < ApplicationController
+
+  def list
+    @clients = Client.all
+  end
+
+  def index 
+    @client = Client.find_by(:id)
+    @projects = @client.projects
+    render 'index'
+  end
+
+  def new
+    @client = Client.new
+  end
+  
+  def create
+    @client = Client.create(client_params)
+      unless @client
+    render "error"
+      else
+        render 'Your client was succesfully created'
+      end
+    end
+  end
+
+  def show
+    @client = Client.find(params[:id])
+    render 'show'
+  end
+  
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+      @client = Client.find(params[:id])
+      respond_to do |format|
+        if @client.update(client_params)
+          format.html { redirect_to @client, notice: 'Task was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
+      end
+  end
+
+  def destroy
+    @client = Client.find(params[:id])
+    @client.destroy
+  end
+
+
+
+  private
+
+    def client_params
+      params.require(:client).permit(:name, :phone, :mail, :contact_name)
+    end
+  
 end
+
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,4 +1,7 @@
 class Asset < ApplicationRecord
   belongs_to :projects
   belongs_to :users
+
+  type = ["presentation","design","Final Art","Client Budget","Provider Budget","Video", "Briefing","Photos"]
+  Asset::type
 end
