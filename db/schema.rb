3f9e875c849866549cbece383c280e0cec304a53
Mariano SDC <mscenzano@gmail.com>
2016-12-08 11:33:45 +0100

    Active record relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208093309) do
+ActiveRecord::Schema.define(version: 20161208103252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "phone"
+    t.string   "email"
+    t.string   "contact_name"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "client_id"
+    t.index ["client_id"], name: "index_projects_on_client_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +52,5 @@ ActiveRecord::Schema.define(version: 20161208093309) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "projects", "clients"
 end
